af8c2c9439412e494fb714c0af6d90eb6843f9d4
FrenkelS <73123197+FrenkelS@users.noreply.github.com>
2024-10-23 22:40:12 +0200

    joti2024 (#3)
    * upgrade dependencies
    
    * COM1 isn't known in the Linux world. Replacing it with SerialPort.getCommPorts()[0]
    
    * different amount of new lines

--- src/main/java/nl/scoutinghilvarenbeek/Printer.java
+++ src/main/java/nl/scoutinghilvarenbeek/Printer.java
@@ -1,6 +1,7 @@
 package nl.scoutinghilvarenbeek;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,27 @@ import com.fazecast.jSerialComm.SerialPort;
 @Component
 public class Printer {
 
-	public void print(String message) {
-		byte[] buffer = message.getBytes(StandardCharsets.US_ASCII);
+	private final SerialPort commPort;
 
-		SerialPort commPort = SerialPort.getCommPort("COM1");
+	public Printer() {
+		SerialPort[] commPorts = SerialPort.getCommPorts();
+
+		if (commPorts.length == 0) {
+			throw new IllegalStateException("No com ports found");
+		}
+
+		if (commPorts.length > 1) {
+			System.out.println("WARNING: Multiple com ports found.");
+			Arrays.stream(commPorts).forEach(System.out::println);
+			System.out.println("Picking the first one: " + commPorts[0]);
+		}
+
+		this.commPort = commPorts[0];
+	}
+
+	public void print(String message) {
+		byte[] buffer = (message + "\n\n\n\n\n\n\n\n\n\n").getBytes(StandardCharsets.US_ASCII);
+
 		commPort.openPort();
 		commPort.writeBytes(buffer, buffer.length);
 		commPort.closePort();
